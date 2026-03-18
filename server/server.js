f2c10ad4eb8ffcb54e31be9a2a7de47c7b5e25fb
sravani <pudusravani123@gmail.com>
2026-03-18 14:21:33 +0530

    feat: integrate Gemini Api and handle rate limit errors

--- server/server.js
+++ server/server.js
@@ -1,22 +1,56 @@
+
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { GoogleGenAI } from "@google/genai";
 dotenv.config();
-const app =express();
+const ai = new GoogleGenAI({
+    apiKey:process.env.GEMINI_API_KEY
+});
+console.log("API KEY:", process.env.GEMINI_API_KEY);
+
+
+const app = express();
+
 app.use(cors());
 app.use(express.json());
-app.post("/api/chat",(req,res)=>{
-    const message=req.body.message;
-    if(!message){
-        return res.status(400).json({error:"Message required"});
+app.post("/api/chat", async (req, res) => {
+  try {
+    const message = req.body.message;
+
+    if (!message) {
+      return res.status(400).json({ error: "Message required" });
     }
+
+    console.log("🔥 USING GEMINI:", message);
+
+    const result = await ai.models.generateContent({
+      model: "gemini-3-flash-preview",
+      contents: message
+    });
+
+    const text =
+      result?.candidates?.[0]?.content?.parts?.[0]?.text;
+
     res.json({
-    reply:"you said: "+ message
+      reply: text || "No response from AI"
+    });
+
+  } catch (error) {
+    console.error("Gemini full error:", error);
+    if(error?.status === 429 ){
+        return res.status(429).json({
+            reply:"Too many requests . please wait a moment."
+        });
+    }
+
+    res.status(500).json({
+      reply: "AI failed"
+    });
+  }
 });
-
-})
-
-
 app.listen(5000,()=>{
-    console.log("server running on port number 5000");
+    console.log("server running on the port 5000");
 })
+
+
